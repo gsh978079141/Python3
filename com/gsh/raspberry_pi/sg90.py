4a0bc593cddc8a607b2d698704052d4bda98cd02
abc978079141 <978079141@qq.com>
2019-05-14 20:32:31 +0800

    sg90

--- com/gsh/raspberry_pi/sg90.py
+++ com/gsh/raspberry_pi/sg90.py
@@ -5,22 +5,22 @@ import atexit
 
 atexit.register(GPIO.cleanup)
 
-servopin = 7
+servopin = 21
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servopin, GPIO.OUT, initial=False)
-p = GPIO.PWM(servopin,50) #50HZ
+p = GPIO.PWM(servopin, 50)  # 50HZ
 p.start(0)
 time.sleep(2)
 
-while(True):
-  for i in range(0,181,10):
-    p.ChangeDutyCycle(2.5 + 10 * i / 180) #设置转动角度
-    time.sleep(0.02)                      #等该20ms周期结束
-    p.ChangeDutyCycle(0)                  #归零信号
-    time.sleep(0.2)
+while (True):
+    for i in range(0, 181, 10):
+        p.ChangeDutyCycle(2.5 + 10 * i / 180)  # 设置转动角度
+        time.sleep(0.02)  # 等该20ms周期结束
+        p.ChangeDutyCycle(0)  # 归零信号
+        time.sleep(0.2)
 
-  for i in range(181,0,-10):
-    p.ChangeDutyCycle(2.5 + 10 * i / 180)
-    time.sleep(0.02)
-    p.ChangeDutyCycle(0)
-    time.sleep(0.2)
+    for i in range(181, 0, -10):
+        p.ChangeDutyCycle(2.5 + 10 * i / 180)
+        time.sleep(0.02)
+        p.ChangeDutyCycle(0)
+        time.sleep(0.2)
