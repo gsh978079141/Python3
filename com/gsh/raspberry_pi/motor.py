2ae4a441b283d0a6bfe8dbc51c69b22bc1c4d21a
abc978079141 <978079141@qq.com>
2019-11-02 09:31:48 +0800

    motor

--- com/gsh/raspberry_pi/motor.py
+++ com/gsh/raspberry_pi/motor.py
@@ -1,25 +1,25 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
-
 ENA = 16
 INT1 = 11
 INT2 = 12
 INT3 = 13
 INT4 = 15
-GPIO.setup(ENA, GPIO.OUT)
-GPIO.setup(INT1, GPIO.OUT)
-GPIO.setup(INT2, GPIO.OUT)
-GPIO.setup(INT3, GPIO.OUT)
-GPIO.setup(INT4, GPIO.OUT)
-####PWM初始化，并设置频率为200HZ####
 # 初始化
-GPIO.setup(ENA, GPIO.OUT)
-# 200HZ
-p1 = GPIO.PWM(ENA, 100)
-p1.start(50)  # 产生占空比为0.4的PWM信号，取值范围0-100
-
+def init():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(ENA, GPIO.OUT)
+    GPIO.setup(INT1, GPIO.OUT)
+    GPIO.setup(INT2, GPIO.OUT)
+    GPIO.setup(INT3, GPIO.OUT)
+    GPIO.setup(INT4, GPIO.OUT)
+    ####PWM初始化，并设置频率为200HZ####
+    # 初始化
+    GPIO.setup(ENA, GPIO.OUT)
+    # 200HZ
+    p1 = GPIO.PWM(ENA, 100)
+    p1.start(50)  # 产生占空比为0.4的PWM信号，取值范围0-100
 
 #########定义电机正转函数##########
 def gogo():
@@ -31,7 +31,6 @@ def gogo():
     GPIO.output(INT4, GPIO.HIGH)
     time.sleep(5)
     stop()
-
 
 
 #########定义电机反转函数##########
@@ -47,12 +46,11 @@ def back():
 
 
 
-
 #########定义电机停止函数##########
 def stop():
     print('停止转动')
     GPIO.output(ENA,GPIO.LOW)
-    p1.stop  # 停止PWM信号
+    # p1.stop  # 停止PWM信号
     GPIO.output(INT1, GPIO.LOW)
     GPIO.output(INT2, GPIO.LOW)
     GPIO.output(INT3, GPIO.LOW)
